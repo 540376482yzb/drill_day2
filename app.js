5c6b1e901f735e3a7670f2ca873673214b982686
Chris Yang <cyang287@yahoo.com>
2018-01-16 12:40:34 -0500

    added filter function

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
+'use strict';
+
 function repeat(fn, num){
-    for(let i = 0; i < num; i++){
-        fn()
-    }
+  for(let i = 0; i < num; i++){
+    fn();
+  }
 }
 
 function hello(){
-    console.log('Hello World')
+  console.log('Hello World');
 }
 
 function goodbye(){
-    console.log('Goodbye World')
+  console.log('Goodbye World');
 }
 
-repeat(hello, 5)
-repeat(goodbye,5)
+repeat(hello, 5);
+repeat(goodbye,5);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, name => name[0] === 'R');
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const j = arr[i];
+    if (fn(j)) {
+      newArray.push(j);
+    }
+  }
+  return newArray;
+}
