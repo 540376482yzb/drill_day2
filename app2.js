c2df273037b7fcf1cd358f80490539e2b120b095
Chris Yang <cyang287@yahoo.com>
2018-01-16 14:31:28 -0500

    started turtleMovements function

--- app2.js
+++ app2.js
@@ -1,18 +1,40 @@
+'use strict';
+
 function hazardWarningCreator(typeOfWarning){
-    let warningCounter = 0
-    return function(location){
-        warningCounter ++
-        console.log(`${typeOfWarning} harzard at ${location}`)
-        console.log(`the ${typeOfWarning} have been triggered ${warningCounter} times today`)
-    }
+  let warningCounter = 0;
+  return function(location){
+    let time = "";
+  if (warningCounter > 0 ) {
+    time = "times"
+  } else {
+    time = "time";
+  }
+    warningCounter ++;
+    console.log(`${typeOfWarning} harzard at ${location}`);
+    console.log(`the ${typeOfWarning} have been triggered ${warningCounter} ${time} today`);
+  };
 }
 
-const first = hazardWarningCreator('Blizzard')
-const second = hazardWarningCreator('Storm')
-const third = hazardWarningCreator('Hurricane')
+const first = hazardWarningCreator('Blizzard');
+const second = hazardWarningCreator('Storm');
+const third = hazardWarningCreator('Hurricane');
 
-first('New York')
-second('Georgia')
-third('Texas')
-third('Texas')
+first('New York');
+second('Georgia');
+third('Texas');
+third('Texas');
+third('Texas');
+
+
+let turtleMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+
+
+const results = turtleMovements.filter(function(move) {
+    if (move[0] >= 0 && move[1] >= 0) {
+      return true;
+    }
+});
+
+console.log(results);
 
